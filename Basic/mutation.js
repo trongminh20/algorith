9b3c783aa0188949a93b3eaa3ac472b5e1137238
minh phan <ptm@minhs-MacBook-Air.local>
2018-04-02 23:46:19 -0700

    update mutation

--- Basic/mutation.js
+++ Basic/mutation.js
@@ -1,18 +1,20 @@
-
 function mutation(arr) {
- var b =[];
- for (var i in arr){
-   b.push(arr[i].toLowerCase());
- } 
- //return b;
-  for(var j = 0; j < b.length; j++){
-    if(b.indexOf(b[j]) !== -1 ){
-      return true;
-    }
-    return false;
-  }
+    var b =[];
+
+    for (var i in arr){
+        b.push(arr[i].toLowerCase());
+    } 
+    
+    //return b;
   
+    for(var j = 0; j < b[1].length; j++){
+        if(b[0].indexOf(b[1][j]) === -1){
+            return false;
+        }
+    }
+        return true;
 }
 
 mutation(["hello", "hey"]);
+
 
