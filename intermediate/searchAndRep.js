fbf91abb17f13261343c1172074b0d0f5de44a7e
Minh Phan <trongminh20@gmail.com>
2018-04-12 01:11:46 -0700

    search and rep

--- intermediate/searchAndRep.js
+++ intermediate/searchAndRep.js
@@ -1,5 +1,12 @@
 function myReplace(str, before, after){
- 
+	var a = str.indexOf(before);
+
+	if(str[a] === str[a].toUpperCase()){
+		let b = after.split('').slice(1).join('');
+
+		after = after.charAt(0).toUpperCase()	 +b;
+	}
+	return str.replace(before,after);
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+console.log(myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped"));
