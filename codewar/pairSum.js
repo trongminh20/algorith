26cc43054cb4adfd1faae4bff5365072ed65b20c
Minh Phan <trongminh20@gmail.com>
2018-04-19 15:04:13 -0700

    pairsum update

--- codewar/pairSum.js
+++ codewar/pairSum.js
@@ -1,25 +1,7 @@
 
 var sum_pairs=function(ints, s){
-    //ints.sort();
-    //var a = ints.reverse();
-    var res = [];
-    var i = 0;
-    while (ints[i]){
-      for(var j = ints.length; j >= i;j--){
-        if(ints[i] + ints[j] === s && i !== j){
-          res.push(ints[i], ints[j]);
-        }
-      }
-      i++;
-    }
-    console.log(ints);
-    return res;
-}
 
-
-
-function test (){
-var a = [
+var n = [
 [1, 4, 8, 7, 3, 15],
 [1, -2, 3, 0, -6, 1],
 [20, -13, 40],
@@ -28,11 +10,34 @@ var a = [
 [4, -2, 3, 3, 4],
 [0, 2, 0],
 [5, 9, 13, -3]
-]
-return a[Math.floor(Math.random()*a.length)];
+];
 
+var b =[8,-6,-7,2,10,8,0,10];
+
+var t =Math.floor(Math.random()*n.length);
+ints = n[t]
+s = b[t];
+
+    var res = [];
+    var i = 0;
+    var len = ints.length;
+    while (ints[i]){
+      for(var j = i++; j <= len; j++){
+        if(ints[i] + ints[j] === s && i!==j ){
+          res.push(ints[i], ints[j]);
+          len =j;
+        }
+      }
+      i++;
+    }
+    console.log(ints,s);
+    return res.length ? res:undefined;
 }
 
-console.log(sum_pairs(test));
+
+
+
+
+console.log(sum_pairs());
 
 
