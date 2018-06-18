d4684782ae1783617350192b4ac44b8c520e3155
Minh Phan <trongminh20@gmail.com>
2018-06-18 00:03:18 -0700

    update information

--- codewar/unique_number.js
+++ codewar/unique_number.js
@@ -1,3 +1,17 @@
+/*
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+It’s guaranteed that array contains more than 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+
+
+
+*/
+
 function findUniq(arr) {
    	return parseFloat(arr.filter(val => arr.indexOf(val) === arr.lastIndexOf(val)));
 
